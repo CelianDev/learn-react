7286a2d3b9c392f8d03c951eb187d30164a4bddf
Cel <celian_ck@proton.me>
2025-08-20 19:51:50 +0200

    small refactor

--- Travel-Journal/src/App.tsx
+++ Travel-Journal/src/App.tsx
@@ -1,7 +1,6 @@
 import Header from "./components/Header";
 import TravelCard from "./components/TravelCard.js";
 import travel from "./data/travel.js";
-import type { TravelCardProps } from "./types/TravelCardProps.js";
 
 function App() {
   return (
@@ -9,7 +8,7 @@ function App() {
       <Header />
       <main className="xl:w-3/5 lg:w-2/3 w-full mx-auto lg:p-10 md:p-4 p-0">
         {travel.map((item) => (
-          <TravelCard key={item.id} {...(item as TravelCardProps)} />
+          <TravelCard key={item.id} {...(item)} />
         ))}
       </main>
     </>

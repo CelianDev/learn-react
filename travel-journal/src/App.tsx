b57fa6c1f9e0e22d662da450867451f203822e0c
Cel <celian_ck@proton.me>
2025-08-20 19:54:35 +0200

    small refactor

--- travel-journal/src/App.tsx
+++ travel-journal/src/App.tsx
@@ -1,4 +1,4 @@
-import Header from "./components/Header";
+import Header from "./components/Header.js";
 import TravelCard from "./components/TravelCard.js";
 import travel from "./data/travel.js";
 
